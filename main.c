f204f79a42ba4a1169d088bd0573ee9857585da4
silvy <oskar.edu.owk@gmail.com>
2025-11-13 17:06:32 +0100

    Add basic window set up file

--- main.c
+++ main.c
@@ -1,14 +1,75 @@
+#include <SDL2/SDL.h>
+#include <stdbool.h>
 
+#define WIDTH 800
+#define HEIGHT 600
 
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("SDL init error: %s", SDL_GetError());
+        return 1;
+    }
 
+    SDL_Window *window = SDL_CreateWindow("Custom Renderer",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          WIDTH, HEIGHT,
+                                          0);
+    if (!window) {
+        SDL_Log("Window creation error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_Texture *texture = SDL_CreateTexture(renderer,
+                                             SDL_PIXELFORMAT_ARGB8888,
+                                             SDL_TEXTUREACCESS_STREAMING,
+                                             WIDTH, HEIGHT);
 
+    bool running = true;
+    SDL_Event event;
 
+    // Example pixel buffer
+    Uint32 *pixels = malloc(WIDTH * HEIGHT * sizeof(Uint32));
 
-int int main(int argc, char *argv[])
-{
+    while (running) {
+        // Handle events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                running = false;
+            else if (event.type == SDL_KEYDOWN) {
+                const Uint8 *state = SDL_GetKeyboardState(NULL);
+                // Meta (Super) + Q to quit
+                if (state[SDL_SCANCODE_LGUI] && event.key.keysym.sym == SDLK_q)
+                    running = false;
+            }
+        }
 
+        // Fill the buffer with a gradient
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                Uint8 r = x % 256;
+                Uint8 g = y % 256;
+                Uint8 b = (x + y) % 256;
+                pixels[y * WIDTH + x] = (255 << 24) | (r << 16) | (g << 8) | b;
+            }
+        }
 
+        // Update texture with new pixels
+        SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(Uint32));
 
-  return EXIT_SUCCESS;
+        // Render
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    free(pixels);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
